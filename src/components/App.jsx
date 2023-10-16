f45ab640af5dbb650bb5078b44b2ec173fe03eb0
Igor Lunyakin <lunyaigor484@gmail.com>
2023-10-17 01:48:06 +0300

    useEffect moved to App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,14 +3,21 @@ import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
 import { StyledAppContainer } from './App.styled';
 import { selectContacts, selectError, selectIsLoading } from 'redux/selectors';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { RotatingLines } from 'react-loader-spinner';
+import { useEffect } from 'react';
+import { fetchContacts } from 'redux/contactOperations';
 
 export const App = () => {
+  const dispatch = useDispatch();
   const isLoading = useSelector(selectIsLoading);
   const error = useSelector(selectError);
   const contacts = useSelector(selectContacts);
   const showLoading = isLoading && !error && contacts.length === 0;
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
 
   return (
     <StyledAppContainer>
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,16 +1,11 @@
 import { ContactListItem } from 'components/ContactListItem/ContactListItem';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectVisibleContacts } from 'redux/selectors';
-import { deleteContact, fetchContacts } from 'redux/contactOperations';
-import { useEffect } from 'react';
+import { deleteContact } from 'redux/contactOperations';
 
 export const ContactList = () => {
   const dispatch = useDispatch();
   const contacts = useSelector(selectVisibleContacts);
-
-  useEffect(() => {
-    dispatch(fetchContacts());
-  }, [dispatch]);
 
   return (
     <ul>
