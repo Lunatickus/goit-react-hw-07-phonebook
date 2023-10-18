d94a5874a08c44b910ca185ae975cf100809618e
Igor Lunyakin <lunyaigor484@gmail.com>
2023-10-18 15:43:55 +0300

    deleteContact changed

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -69,15 +69,14 @@ const contactsSlice = createSlice({
       .addCase(addContact.fulfilled, (state, action) => {
         state.isLoading = false;
         state.error = null;
-        state.items.push(action.payload);
+        state.items = [...state.items, action.payload];
       })
       // .addCase(addContact.rejected, handleRejected)
       // .addCase(deleteContact.pending, handlePending)
       .addCase(deleteContact.fulfilled, (state, action) => {
         state.isLoading = false;
         state.error = null;
-        const index = state.items.findIndex(item => item.id === action.payload.id);
-        state.items.splice(index, 1);
+        state.items = state.items.filter(item => item.id !== action.payload.id)
       })
       // .addCase(deleteContact.rejected, handleRejected)
       .addMatcher(isPendingAction, handlePending)
